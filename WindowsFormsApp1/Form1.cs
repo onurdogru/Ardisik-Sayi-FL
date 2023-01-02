d908080bb514d480cc2400ca825aa9e6b274bf26
Onur <onurdogruiletisim@gmail.com>
2023-01-02 18:16:13 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                toplam = toplam + i;
+            }
+            label1.Text = toplam.ToString();
+
+
+
+            //breakpoint için;
+
+            for (int i = 1; i < 6; i+=1)
+            {
+                listBox1.Items.Add(i);
+            }
+
+        }
+
+    }
+}
